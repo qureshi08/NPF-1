253c1acb123da877c7b0798c4c19361bd5a00fef
NEW_USERNAME <NEW_EMAIL>
2025-11-28 13:38:16 +0500

    CRITICAL FIX: Remove auto database wipe on deployment - preserve production data

--- build.sh
+++ build.sh
@@ -3,4 +3,6 @@
 set -o errexit
 
 pip install -r requirements.txt
-python init_db.py
+
+# Database initialization removed - use /init-database-secret-2024 endpoint for first-time setup
+# DO NOT run init_db.py here as it will wipe all production data on every deployment!
